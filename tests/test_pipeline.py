38497eb73bcd461ab6a3b877097236d743bcc4c6
marieabdlk-art <marie.abdlk@gmail.com>
2026-05-15 17:27:45 +0200

    Add shape selector pipeline test

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -74,6 +74,45 @@ def test_delete_smallest():
                                           [0, 0, 0, 0, 0]]
 
 
+def test_delete_single_pixels_shape_selector():
+    result = run_pipeline(
+        task_id="delete_single_pixels",
+        train_pairs=[
+            (
+                [[0, 0, 0, 0, 0, 0],
+                 [0, 2, 2, 0, 3, 0],
+                 [0, 2, 2, 0, 0, 0],
+                 [0, 0, 0, 4, 0, 0]],
+                [[0, 0, 0, 0, 0, 0],
+                 [0, 2, 2, 0, 0, 0],
+                 [0, 2, 2, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0]],
+            ),
+            (
+                [[0, 5, 0, 0, 0, 0],
+                 [0, 0, 2, 2, 2, 0],
+                 [0, 0, 2, 2, 2, 0],
+                 [0, 0, 0, 0, 0, 6]],
+                [[0, 0, 0, 0, 0, 0],
+                 [0, 0, 2, 2, 2, 0],
+                 [0, 0, 2, 2, 2, 0],
+                 [0, 0, 0, 0, 0, 0]],
+            ),
+        ],
+        test_input=[[0, 0, 7, 0, 0, 0],
+                    [0, 2, 2, 2, 0, 0],
+                    [0, 2, 2, 2, 0, 8],
+                    [0, 0, 0, 0, 0, 0]],
+    )
+    assert result.status == "solved"
+    assert result.generator == "DeleteGenerator"
+    assert "shape=single_pixel" in result.program
+    assert result.prediction.tolist() == [[0, 0, 0, 0, 0, 0],
+                                          [0, 2, 2, 2, 0, 0],
+                                          [0, 2, 2, 2, 0, 0],
+                                          [0, 0, 0, 0, 0, 0]]
+
+
 def test_fill_bbox_largest():
     result = run_pipeline(
         task_id="fill_bbox_largest",
